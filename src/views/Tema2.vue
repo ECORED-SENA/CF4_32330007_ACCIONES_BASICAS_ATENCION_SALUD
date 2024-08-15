615bea059b4294578715dd044eac90b782016b12
Camilo <camilobr17@gmail.com>
2024-08-15 09:23:56 -0500

    tema2_listo

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,76 +5,60 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Cinemática del trauma 
-    .row.justify-content-center.align-items-center.mb-5
-      .col-lg-8.mb-lg-0.mb-3(data-aos="fade-right")
-        p.mb-0 La cinemática del trauma es entender y analizar la escena de un accidente para determinar las posibles lesiones de los pacientes y darles un tratamiento más rápido y efectivo. Los datos obtenidos en la escena tanto del entorno como del paciente deben registrarse y transmitirse al personal de salud encargado del evento para orientar sobre las posibles lesiones que se pueden presentar. Se deben buscar objetos libres dentro del vehículo, ya que pudieron lesionar directamente a los ocupantes al comportarse como verdaderos proyectiles.
-      .col-lg-4.col-7(data-aos="fade-left"): img(src='@/assets/curso/temas/29.png', alt='')
-    .titulo-tres: h3 Vehículo - objeto 
-    p.mb-5(data-aos='fade-right') Las lesiones dependerán del lugar ocupado por cada persona dentro del vehículo (conductor, copiloto, parte trasera), del uso o no del cinturón de seguridad o sistemas de retención infantil, de la activación del airbag y de la fuerza del impacto. 
-    .titulo-tres: h3 Ocupante - parabrisas/volante 
-    p.mb-5(data-aos='fade-right') Durante un accidente de tránsito, los ocupantes de un vehículo pueden experimentar dos tipos principales de desplazamiento que resultan en lesiones severas. 
+      h1 Luxaciones
+    p.mb-5(data-aos='fade-right') Una luxación es la separación de dos huesos de la articulación que los sostiene, causada generalmente por una fuerza que desgarra los ligamentos. Es muy dolorosa y la extremidad pierde funcionalidad. Los síntomas incluyen:
     .row.justify-content-center.mb-5
-      .col-lg-5.mb-lg-0.mb-3
-        .p-4(style="background-color: #d7f38d").h-100
-          .row.justify-content-center.mb-3
-            .col-lg-8
-              img(src='@/assets/curso/temas/31.svg', alt='')
-          .tarjeta.p-4(style="background-color: #f6fce7 ")
-            h5.text-center Desplazamiento arriba y encima
-            p.mb-0 Donde el cuerpo tiende a salir en dirección oblicua y hacia arriba. La cabeza impacta con el parabrisas, retrovisor, marco interno metálico, produciendo fracturas craneales,  faciales, lesiones encefálicas y diferentes tipos de herida
-      .col-lg-5.mb-lg-0.mb-3
-        .p-4(style="background-color: #b1d1f9").h-100
-          .row.justify-content-center.mb-3
-            .col-lg-8
-              img(src='@/assets/curso/temas/32.svg', alt='')
-          .tarjeta.p-4(style="background-color: #d7e8fc ")
-            h5.text-center Desplazamiento arriba y encima
-            p.mb-0 Donde el cuerpo tiende a salir en dirección oblicua y hacia arriba. La cabeza impacta con el parabrisas, retrovisor, marco interno metálico, produciendo fracturas craneales,  faciales, lesiones encefálicas y diferentes tipos de herida.
-    .titulo-tres: h3 Órganos internos
-    .row.justify-content-center.align-items-center.mb-5
-      .col-lg-8.mb-lg-0.mb-3(data-aos="fade-right")
-        p.mb-0 Las lesiones más frecuentes son: fracturas costales en el hemitórax golpeado, pudiendo aparecer lesiones intratorácicas asociadas, fracturas de pelvis, lesiones craneoencefálicas por el brusco movimiento de la cabeza hacia el vehículo golpeando contra estructuras internas, rotura hepática o esplénica por golpe directo y a consecuencia de fracturas costales bajas (9ª a 12ª). 
-      .col-lg-4.col-6(data-aos="fade-left"): img(src='@/assets/curso/temas/33.png', alt='')
-    .titulo-tres: h3 Motos
-    p.mb-5(data-aos='fade-right') Las lesiones más habituales son: erosiones, contusiones o fracturas de miembros inferiores que se pueden producir por golpe con otro vehículo o por el golpe contra el suelo al caer sobre él con o sin deslizamiento. Contra un obstáculo fijo, el conductor saldrá proyectado por el manillar, golpeando este con ambos muslos, produciendo fracturas y lesiones de columna torácica.
+      .col-lg-6.col-7.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/14.png', alt='')
+      .col-lg-6
+        .row.justify-content-center.mb-3
+          .col-lg-6
+            .tarjeta.p-4.tarjeta-hover
+              .row.justify-content-center.align-items-center
+                .col-2.col-lg-4: img(src='@/assets/curso/temas/13.svg', alt='')
+                .col-lg-8
+                  p.mb-0 Inflamación
+          .col-lg-6
+            .tarjeta.p-4.tarjeta-hover
+              .row.justify-content-center.align-items-center
+                .col-2.col-lg-4: img(src='@/assets/curso/temas/13.svg', alt='')
+                .col-lg-8
+                  p.mb-0 Prominencia ósea o depresión visible
+        .row.justify-content-center.mb-3
+          .col-lg-6
+            .tarjeta.p-4.tarjeta-hover
+              .row.justify-content-center.align-items-center
+                .col-2.col-lg-4: img(src='@/assets/curso/temas/13.svg', alt='')
+                .col-lg-8
+                  p.mb-0 Dolor moderado
+          .col-lg-6
+            .tarjeta.p-4.tarjeta-hover
+              .row.justify-content-center.align-items-center
+                .col-2.col-lg-4: img(src='@/assets/curso/temas/13.svg', alt='')
+                .col-lg-8
+                  p.mb-0 Pérdida de funcionalidad de la extremidad
+    .titulo-tres: h3 Manejo inicial de luxaciones
+    p.mb-5(data-aos='fade-right') El primer respondiente debe: 
     .row.justify-content-center.mb-5
-      .col-lg-10 
-        .p-5(style="background-color: #eefad1 ")
-          .row.justify-content-center
-            .col-lg-6.col-7.mb-lg-0.mb-3: img(src='@/assets/curso/temas/34.png', alt='')
-            .col-lg-6
-              ul.lista-ul
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Lesiones en genitales y vías urinarias bajas por golpe contra el depósito de combustible.
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 La clavícula también puede fracturarse por el impacto del casco al caer el motorista.
-
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Es muy frecuente encontrar abrasiones y heridas cutáneas por rozamiento y desgarros amplios de piel.
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Son importantes los traumatismos craneoencefálicos y faciales, en los que se pueden encontrar lesiones como erosiones o desgarros del cuero cabelludo, fracturas craneales y lesiones cerebrales.
-    .titulo-tres: h3 Peatón 
-    p.mb-5(data-aos='fade-right') Durante un accidente de tránsito, los ocupantes de un vehículo pueden experimentar diversos tipos de desplazamientos que resultan en lesiones severas. Estos desplazamientos incluyen: 
-    .row.justify-content-center.mb-3.g-0
-      .col-lg-2.col-7.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/35.png', alt='')
-      .col-lg-6(data-aos="fade-left")
-        .p-4.h-100(style="background-color: #ffe0cb ").d-flex.align-items-center
-          p.mb-0 El primer impacto del paragolpes con alguna parte del cuerpo.
-    .row.justify-content-center.mb-3.g-0
-      .col-lg-2.col-7.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/36.png', alt='')
-      .col-lg-6(data-aos="fade-left")
-        .p-4.h-100(style="background-color: #ffe0cb ").d-flex.align-items-center
-          p.mb-0 El segundo impacto en adultos y niños de talla grande se producirá por el impacto de la cadera con el borde del capó, golpeando el tórax contra este, los hombros y la cabeza contra el parabrisas.
-    .row.justify-content-center.mb-5.g-0
-      .col-lg-2.col-7.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/37.png', alt='')
-      .col-lg-6(data-aos="fade-left")
-        .p-4.h-100(style="background-color: #ffe0cb ").d-flex.align-items-center
-          p.mb-0 El tercer impacto será producido al caer contra el suelo.
+      .col-lg-3.mb-lg-0.mb-3
+        .tarjeta.p-4.color-secundario.h-100 
+          .row.justify-content-center.mb-5
+            .col-5: img(src='@/assets/curso/temas/15.svg', alt='')
+          p.text-center Inmovilizar la articulación afectada
+      .col-lg-3.mb-lg-0.mb-3
+        .tarjeta.p-4.color-secundario.h-100 
+          .row.justify-content-center.mb-5
+            .col-5: img(src='@/assets/curso/temas/15.svg', alt='')
+          p.text-center Colocar al lesionado en una posición cómoda hasta su traslado, para evitar dolor.
+      .col-lg-3.mb-lg-0.mb-3
+        .tarjeta.p-4.color-secundario.h-100 
+          .row.justify-content-center.mb-5
+            .col-5: img(src='@/assets/curso/temas/15.svg', alt='')
+          p.text-center Colocar al lesionado en una posición cómoda hasta su traslado, para evitar dolor.
+      .col-lg-3.mb-lg-0.mb-3
+        .tarjeta.p-4.color-secundario.h-100 
+          .row.justify-content-center.mb-5
+            .col-5: img(src='@/assets/curso/temas/15.svg', alt='')
+          p.text-center Colocar al lesionado en una posición cómoda hasta su traslado, para evitar dolor.
 
 
 
