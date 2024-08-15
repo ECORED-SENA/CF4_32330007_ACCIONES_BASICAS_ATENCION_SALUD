0d7cb80ef7a33a7a62652b97bb180dc4efafba9a
Camilo <camilobr17@gmail.com>
2024-08-15 08:24:17 -0500

    Avances tema1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -6,7 +6,83 @@
       .titulo-principal__numero
         span 1
       h1 Lesiones osteomusculares
-    
+    .row.justify-content-center.mb-5
+      .col-lg-9.mb-lg-0.mb-3(data-aos="fade-right")
+        .row.justify-content-center.g-0
+          .col-lg-5.col-6.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/3.png', alt='').w-100
+          .col-lg-7
+            .p-5(style="background-color: #d7f38d ").h-100
+              p.mb-0 Las lesiones osteomusculares incluyen daños en los huesos, las articulaciones y los músculos. Es crucial tratarlas adecuadamente para evitar lesiones permanentes. Las principales lesiones del sistema osteomuscular son fracturas, esguinces y luxaciones.
+    Separador
+    #t_1_1.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2 1.1 Fracturas
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6.col-7(data-aos="fade-right"): img.w-100(src='@/assets/curso/temas/4.png', alt='')
+      .col-lg-6(data-aos="fade-left")
+        .tarjeta.p-5(style="background-color: #d9e9fc ")
+          p.mb-4 Una fractura es la pérdida de continuidad de un hueso, que puede romperse parcial o totalmente. Esto ocurre generalmente debido a una fuerza significativa, aunque enfermedades como la osteoporosis y la deficiencia de calcio también pueden debilitar los huesos, aumentando el riesgo de fracturas.
+          .row.justify-content-end
+            .col-2: img(src='@/assets/curso/temas/5.svg', alt='')
+    p.mb-5(data-aos='fade-right') 
+      span.azul El trauma osteomuscular puede ser potencialmente mortal si provoca hemorragias internas o externas. Las fracturas se clasifican en:
+    .row.justify-content-center.mb-5
+      .col-lg-6.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/6.png", alt="alt")
+          .crd_hover_txt--body.text-center.w-100(style="background-color: #2b63a8")
+            h4.mb-3.text-white Fractura cerrada
+            p.mb-0.text-white 	El hueso se fractura sin perforar la piel, por lo que no hay lesiones cutáneas.
+      .col-lg-6.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/7.png", alt="alt")
+          .crd_hover_txt--body.text-center.w-100(style="background-color: #eefad1")
+            h4.mb-3 Fractura abierta
+            p.mb-0 	El hueso fracturado perfora la piel, lesionando los tejidos blandos. El riesgo principal es la hemorragia, ya que los vasos sanguíneos pueden dañarse, lo que requiere control inmediato para evitar una descompensación mayor del paciente.
+    .titulo-tres: h3 Tipos de fracturas
+    p.mb-5(data-aos='fade-right') Las fracturas son lesiones comunes que pueden afectar a personas de todas las edades. Se clasifican según la naturaleza de la ruptura y su relación con el eje del hueso. A continuación, se detallan algunos de los tipos más comunes de fracturas:
+    .fondo-slyder.p-5.mb-5
+      .tarjeta.bg-white.p-5
+        SlyderA(tipo='b')
+          .row.justify-content-center
+            .col-lg-6.mb-4.mb-md-0
+              h4 Fractura de tallo verde
+              p Es una fractura incompleta en la que el hueso no se rompe completamente, característica de los huesos más flexibles de los niños.
+            .col-lg-5
+              figure
+                img(src='@/assets/curso/temas/8.svg', alt='Texto que describa la imagen')
+          .row.justify-content-center
+            .col-lg-6.mb-4.mb-md-0
+              h4 Fractura transversal
+              p Este tipo de fractura se presenta en un ángulo recto respecto al eje del hueso, creando una ruptura limpia y directa.
+            .col-lg-5
+              figure
+                img(src='@/assets/curso/temas/9.svg', alt='Texto que describa la imagen')
+          .row.justify-content-center
+            .col-lg-6.mb-4.mb-md-0
+              h4 Fractura oblicua
+              p Se distingue por una ruptura que forma un ángulo respecto al eje de la médula, lo que puede provocar una lesión menos estable y más compleja de tratar.
+            .col-lg-5
+              figure
+                img(src='@/assets/curso/temas/10.svg', alt='Texto que describa la imagen')
+          .row.justify-content-center
+            .col-lg-6.mb-4.mb-md-0
+              h4 Fractura conminuta
+              p En esta fractura, el hueso se rompe en varios pedazos, lo que suele requerir una intervención médica más compleja y puede tener un período de recuperación prolongado.
+            .col-lg-5
+              figure
+                img(src='@/assets/curso/temas/11.svg', alt='Texto que describa la imagen')
+    p.mb-5(data-aos='fade-right') Los síntomas de una fractura son: 
+    .row.justify-content-center.mb-5
+      .col-lg-6.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/12.png', alt='')
+      .col-lg-6(data-aos="fade-left")
+        .row.justify-content-center.mb-5
+          .col-lg-4
+            .tarjeta.p-
+
 
 
 
