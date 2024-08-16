e5863672437a8aa8d19cc2c53c1fcd3cb42856bf
elrilacho <camilobr17@gmail.com>
2024-08-16 00:36:41 -0500

    tema5_listo

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -1,199 +1,121 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.fondo-3
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.fondo-5
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
-        span 3
-      h1 Botiquín de primeros auxilios 
+        span 5
+      h1  Heridas
+    p.mb-5(data-aos='fade-right') Una herida es la pérdida de continuidad de la piel, generalmente causada por traumatismos. Las heridas pueden clasificarse en abiertas y cerradas, dependiendo de si los tejidos subyacentes quedan expuestos. #[span.azul El manejo adecuado de las heridas es crucial para prevenir infecciones y promover una cicatrización rápida y efectiva. #[br] Los tipos de heridas son: ]
     .row.justify-content-center.mb-5
-      .col-lg-8.mb-lg-0.mb-3(data-aos="fade-right")
-        p.mb-0 Los botiquines son un recurso indispensable para los primeros auxiliadores, en ellos están todos los elementos necesarios para la atención del paciente. Hay varios tipos de botiquines. A continuación, se darán sugerencias para algunos de ellos. Recuerde que el tipo de botiquín dependerá del entrenamiento y del rol de los auxiliadores o del lugar donde se encuentren. 
-      .col-lg-4.col-7: img(src='@/assets/curso/temas/38.png', alt='')
-    .titulo-tres: h3 Botiquín para el vehículo 
-    p.mb-5 Las lesiones dependerán del lugar ocupado por cada persona dentro del vehículo (conductor, copiloto, parte trasera), del uso o no del cinturón de seguridad o sistemas de retención infantil, de la activación del airbag y de la fuerza del impacto. 
-    .row.justify-content-center.mb-5
-      .col-lg-10 
-        .p-5(style="background-color: #eefad1 ")
-          .row.justify-content-center
-            .col-lg-6.col-7.mb-lg-0.mb-3: img(src='@/assets/curso/temas/39.png', alt='')
-            .col-lg-6
-              ul.lista-ul
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Antisépticos 
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Tijeras cortatodo
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Algodón 
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Gasa estéril 
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Esparadrapo o vendas adhesivas 
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Venda elástica
-                li.d-flex
-                  i.fas.fa-circle
-                  p.mb-0 Jabón 
-    .titulo-tres: h3 Botiquín básico 
-    p.mb-5(data-aos='fade-right') Un botiquín básico es esencial para proporcionar atención inmediata en situaciones de emergencia. Debe contener los siguientes elementos: 
-    .row.justify-content-center.mb-5
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-lg-7: img(src='@/assets/curso/temas/40.svg', alt='')
-          p.text-center Termometro
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-lg-7: img(src='@/assets/curso/temas/41.svg', alt='')
-          p.text-center Tijeras de cortado
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-5: img(src='@/assets/curso/temas/42.svg', alt='')
-          p.text-center Gasas o apósitos
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-6: img(src='@/assets/curso/temas/43.svg', alt='')
-          p.text-center Algodón
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-6: img(src='@/assets/curso/temas/44.svg', alt='')
-          p.text-center Vendas de gasa 
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-5: img(src='@/assets/curso/temas/45.svg', alt='')
-          p.text-center Aplicadores de algodón
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-5: img(src='@/assets/curso/temas/46.svg', alt='')
-          p.text-center Curitas de diferentes tamaños
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-4: img(src='@/assets/curso/temas/47.svg', alt='')
-          p.text-center Alcohol antiséptico
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-lg-7: img(src='@/assets/curso/temas/48.svg', alt='')
-          p.text-center Jabón antiséptico
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-6: img(src='@/assets/curso/temas/49.svg', alt='')
-          p.text-center Gafas de bioseguridad
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-6: img(src='@/assets/curso/temas/50.svg', alt='')
-          p.text-center Tapabocas o mascarillas
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-6: img(src='@/assets/curso/temas/51.svg', alt='')
-          p.text-center Guantes de látex
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-6: img(src='@/assets/curso/temas/52.svg', alt='')
-          p.text-center Protector solar
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-5: img(src='@/assets/curso/temas/53.svg', alt='')
-          p.text-center Repelente
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-6: img(src='@/assets/curso/temas/54.svg', alt='')
-          p.text-center Sueros de hidratación oral
-      .col-2.mb-3
-        .tarjeta.p-4(style="background-color: #eefad1 ").h-100
-          .row.justify-content-center.mb-3
-            .col-7: img(src='@/assets/curso/temas/55.svg', alt='')
-          p.text-center Microporo
-    .titulo-tres: h3 Botiquín empresarial 
-    p.mb-5 En caso de accidente, los lugares de trabajo deben disponer de material de primeros auxilios en cantidad suficiente para cubrir el número de trabajadores de la empresa y los riesgos a los que puedan estar expuestos. La distribución y situación del material debe garantizar su accesibilidad para una atención rápida. El material se revisará periódicamente y se irá reponiendo tan pronto como se utilice o se encuentre vencido. El tamaño y los elementos que el botiquín contenga dependerán del entrenamiento y el rol del personal que lo usará, según la legislación vigente del país. Elementos recomendados según entrenamiento y necesidad de la empresa: 
-      br
-      br
-      | #[b Ademas de los elementos básicos debe tener : ]
-    .row.justify-content-center.mb-5.g-0 
-      .col-lg-5.col-7.mb-lg-0.mb-3: img(src='@/assets/curso/temas/56.png', alt='')
-      .col-lg-7
-        .p-4.h-100(style="background-color: #d7f38d ")
-          .row.justify-content-center
-            .col-lg-6.mb-lg-0.mb-3
-              ul.lista-ul
-                .tarjeta.p-1(style="background-color: #eefad1").mb-0
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Directorio de ambulancias y servicios de salud o teléfonos de emergencias locales
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Repelentes de insectos
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Tensiometro
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Fonendoscopio
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Glucómetro y tirillas
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Collarines o collar cervical
-            .col-lg-6.mb-lg-0.mb-3
-              ul.lista-ul
-                .tarjeta.p-1(style="background-color: #eefad1").mb-0
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Camilla o tabla de espina larga
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Silla de ruedas
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Mascarillas de reanimación
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Guantes de látex y vinilo
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Cánulas orofaríngeas
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Férulas
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Bolsas de frio instantanea
-                .tarjeta.p-1(style="background-color: #eefad1").mb-1
-                  li.d-flex.mb-0
-                    i.fas.fa-circle
-                    p.mb-0 Solución salina 0,9%
+      .col-lg-6.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/31.png", alt="alt")
+          .crd_hover_txt--body.text-center.w-100(style="background-color: #2b63a8")
+            h4.mb-3.text-white Heridas abiertas
+            p.mb-0.text-white Separación de tejidos blandos, con riesgo de infección.
+      .col-lg-6.col-md-9.col-11.mb-4.mb-xl-0
+        .crd_hover_txt(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/temas/32.png", alt="alt")
+          .crd_hover_txt--body.text-center.w-100(style="background-color: #eefad1")
+            h4.mb-3 Heridas cerradas
+            p.mb-0 	Lesiones internas no visibles, pueden comprometer órganos y causar hemorragias graves.
+    h4 La clasificación de heridas
+    p.mb-5(data-aos='fade-right') Las heridas se clasifican según su naturaleza y las características de los tejidos afectados. A continuación, se presentan los tipos principales de heridas:
+    //TODO! PENDIENTES CARDS 
 
+    .titulo-tres: h3 Manejo de heridas leves
+    p.mb-5(data-aos='fade-right') El manejo adecuado de las heridas leves es esencial para prevenir infecciones y promover una cicatrización rápida y efectiva. #[span.azul A continuación, se detallan los pasos básicos para tratar heridas leves:]   
+    .fondo-slyder2.p-5.mb-4
+      .row.justify-content-center.align-items-center
+        .col-lg-4.col-7.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/33.png', alt='') 
+        .col-lg-8(data-aos="fade-left")
+          SlyderF.mb-5(columnas="col-lg-6 col-xl-6")
+            .tarjeta.bg-white.p-4.h-100
+              .row.justify-content-center.mb-3
+                .col-6
+                  img(src='@/assets/curso/temas/34.svg' alt='AvatarTop')
+              .d-flex.justify-content-center
+                .d-inline-block.p-2(style="background-color: #ffb27e")
+                  h5.mb-0 01
+              p.text-center Lavar las manos antes de atender la herida.
+            .tarjeta.bg-white.p-4.h-100
+              .row.justify-content-center.mb-3
+                .col-6
+                  img(src='@/assets/curso/temas/35.svg' alt='AvatarTop')
+              .d-flex.justify-content-center
+                .d-inline-block.p-2(style="background-color: #ffb27e")
+                  h5.mb-0 02
+              p.text-center Detener la hemorragia con presión directa usando un apósito limpio.
+            .tarjeta.bg-white.p-4.h-100
+              .row.justify-content-center.mb-3
+                .col-6
+                  img(src='@/assets/curso/temas/36.svg' alt='AvatarTop')
+              .d-flex.justify-content-center
+                .d-inline-block.p-2(style="background-color: #ffb27e")
+                  h5.mb-0 03
+              p.text-center Detener la hemorragia con presión directa usando un apósito limpio.
+            .tarjeta.bg-white.p-4.h-100
+              .row.justify-content-center.mb-3
+                .col-6
+                  img(src='@/assets/curso/temas/37.svg' alt='AvatarTop')
+              .d-flex.justify-content-center
+                .d-inline-block.p-2(style="background-color: #ffb27e")
+                  h5.mb-0 04
+              p.text-center Levantar la extremidad para reducir el sangrado.
+            .tarjeta.bg-white.p-4.h-100
+              .row.justify-content-center.mb-3
+                .col-6
+                  img(src='@/assets/curso/temas/38.svg' alt='AvatarTop')
+              .d-flex.justify-content-center
+                .d-inline-block.p-2(style="background-color: #ffb27e")
+                  h5.mb-0 05
+              p.text-center No aplicar cremas ni polvos en la herida.
+            .tarjeta.bg-white.p-4.h-100
+              .row.justify-content-center.mb-3
+                .col-6
+                  img(src='@/assets/curso/temas/39.svg' alt='AvatarTop')
+              .d-flex.justify-content-center
+                .d-inline-block.p-2(style="background-color: #ffb27e")
+                  h5.mb-0 06
+              p.text-center Lavar la herida diariamente con agua, secar bien y cubrir si es necesario.
+    .titulo-tres: h3 Manejo de heridas leves
+    p.mb-5(data-aos='fade-right') El manejo de heridas graves requiere una atención inmediata y cuidadosa para evitar complicaciones y salvar la vida del paciente. #[span.azul Los siguientes pasos son esenciales para tratar heridas graves: ]
+    PasosA.color-acento-contenido.mb-5(tipo="n")
+      .row(titulo="")
+        .col-lg-9.mb-4.mb-lg-0
+          h4 Paso 1
+          p Evaluar el tipo de herida.
+        .col-lg-2.col-4: img(src='@/assets/curso/temas/40.svg', alt='')
+      .row(titulo="")
+        .col-lg-2.col-4.mb-4.mb-lg-0: img(src='@/assets/curso/temas/41.svg', alt='')
+        .col-lg-9
+          h4 Paso 2
+          p Detener la hemorragia con presión directa usando un apósito limpio.
+      .row(titulo="")
+        .col-lg-9.mb-4.mb-lg-0
+          h4 Paso 3
+          p Levantar la extremidad para reducir el sangrado.
+        .col-lg-2.col-4: img(src='@/assets/curso/temas/42.svg', alt='')
+      .row(titulo="")
+        .col-lg-8.col-4.mb-4.mb-lg-0
+          h4 Paso 4
+          p Solicitar atención médica urgente.
+        .col-lg-2.col-4: img(src='@/assets/curso/temas/43.svg', alt='')
+      .row(titulo="")
+        .col-lg-2.col-4.mb-4.mb-lg-0: img(src='@/assets/curso/temas/44.svg', alt='')
+        .col-lg-9
+          h4 Paso 5
+          p Mantener la calma y pedir ayuda para el traslado.
+      .row(titulo="")
+        .col-lg-9.mb-4.mb-lg-0
+          h4 Paso 6
+          p Cubrir la herida y controlar la hemorragia durante el traslado.
+        .col-lg-2.col-4: img(src='@/assets/curso/temas/45.svg', alt='')
 
 </template>
 
